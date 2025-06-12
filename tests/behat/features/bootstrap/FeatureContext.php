bf12c96a72371bca8ff97a4abb2d76de8e0d1b74
Yannick Warnier <ywarnier@beeznest.org>
2025-06-12 11:01:56 +0200

    CI: Increase 'very long time' to 10s temporarily until interface is optimized

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -460,7 +460,7 @@ class FeatureContext extends MinkContext
     public function waitVeryLongForThePageToBeLoaded()
     {
         //$this->getSession()->wait(10000, "document.readyState === 'complete'");
-        $this->getSession()->wait(7000);
+        $this->getSession()->wait(10000);
     }
 
     /**
