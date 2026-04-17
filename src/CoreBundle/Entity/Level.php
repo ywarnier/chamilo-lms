f1a8ab9eaeecf3b03da370755b49a4a547bf9ecd
Yannick Warnier <ywarnier@beeznest.org>
2026-04-17 12:02:10 +0200

    API: Add API support for Skill levels and level profiles

--- src/CoreBundle/Entity/Level.php
+++ src/CoreBundle/Entity/Level.php
@@ -6,33 +6,58 @@ declare(strict_types=1);
 
 namespace Chamilo\CoreBundle\Entity;
 
+// Chamilo HR extension
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Stringable;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * Skill level.
- */
+#[ApiResource(
+    operations: [
+        new GetCollection(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+        new Get(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+        new Post(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+        new Put(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+        new Delete(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+    ],
+    normalizationContext: ['groups' => ['skill_level:read']],
+    denormalizationContext: ['groups' => ['skill_level:write']],
+)]
 #[ORM\Table(name: 'skill_level')]
 #[ORM\Entity]
 class Level implements Stringable
 {
+    #[Groups(['skill_level:read', 'skill_level_profile:read'])]
     #[ORM\Column(name: 'id', type: 'integer')]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     protected ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Groups(['skill_level:read', 'skill_level:write', 'skill_level_profile:read'])]
     #[ORM\Column(name: 'title', type: 'string', length: 255, nullable: false)]
     protected string $title;
 
     #[Gedmo\SortablePosition]
+    #[Groups(['skill_level:read', 'skill_level:write', 'skill_level_profile:read'])]
     #[ORM\Column(name: 'position', type: 'integer')]
     protected int $position;
 
+    #[Assert\NotBlank]
+    #[Groups(['skill_level:read', 'skill_level:write', 'skill_level_profile:read'])]
     #[ORM\Column(name: 'short_title', type: 'string', length: 255, nullable: false)]
     protected string $shortTitle;
 
     #[Gedmo\SortableGroup]
+    #[Groups(['skill_level:read', 'skill_level:write'])]
     #[ORM\ManyToOne(targetEntity: SkillLevelProfile::class, inversedBy: 'levels')]
     #[ORM\JoinColumn(name: 'profile_id', referencedColumnName: 'id')]
     protected ?SkillLevelProfile $profile = null;
--- src/CoreBundle/Entity/SkillLevelProfile.php
+++ src/CoreBundle/Entity/SkillLevelProfile.php
@@ -6,20 +6,44 @@ declare(strict_types=1);
 
 namespace Chamilo\CoreBundle\Entity;
 
+// Chamilo HR extension
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Stringable;
+use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
+#[ApiResource(
+    operations: [
+        new GetCollection(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+        new Get(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+        new Post(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+        new Put(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+        new Delete(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_HR')"),
+    ],
+    normalizationContext: ['groups' => ['skill_level_profile:read']],
+    denormalizationContext: ['groups' => ['skill_level_profile:write']],
+)]
 #[ORM\Table(name: 'skill_level_profile')]
 #[ORM\Entity]
 class SkillLevelProfile implements Stringable
 {
+    #[Groups(['skill_level_profile:read'])]
     #[ORM\Column(name: 'id', type: 'integer')]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     protected ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Groups(['skill_level_profile:read', 'skill_level_profile:write'])]
     #[ORM\Column(name: 'title', type: 'string', length: 255, nullable: false)]
     protected string $title;
 
@@ -32,6 +56,7 @@ class SkillLevelProfile implements Stringable
     /**
      * @var Collection<int, Level>
      */
+    #[Groups(['skill_level_profile:read'])]
     #[ORM\OneToMany(mappedBy: 'profile', targetEntity: Level::class, cascade: ['persist'])]
     #[ORM\OrderBy(['position' => 'ASC'])]
     protected Collection $levels;
