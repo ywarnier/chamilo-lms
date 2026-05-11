3576c07827d02e25b8558445cdd6a268e767b916
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-05-11 15:17:29 -0500

    HR: Update migration namespaces from V200 to HR

--- src/CoreBundle/Migrations/Schema/HR/Version20260417120100.php
+++ src/CoreBundle/Migrations/Schema/HR/Version20260417120100.php
@@ -4,7 +4,7 @@
 
 declare(strict_types=1);
 
-namespace Chamilo\CoreBundle\Migrations\Schema\V200;
+namespace Chamilo\CoreBundle\Migrations\Schema\HR;
 
 use Chamilo\CoreBundle\Migrations\AbstractMigrationChamilo;
 use Doctrine\DBAL\Schema\Schema;
